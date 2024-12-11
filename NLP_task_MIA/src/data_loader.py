dea4521b72d4eeeb425bddc6e867073bb0d64175
Shaista Shabbir <shaista.shabbir@hitec-hamburg.de>
2024-12-12 00:14:35 +0100

    hard coded moved to settings

--- NLP_task_MIA/src/data_loader.py
+++ NLP_task_MIA/src/data_loader.py
@@ -1,17 +1,32 @@
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
+from typing import Tuple
+import numpy as np
+from utils.settings import settings
 
-MAX_FEATURES = 20000
-MAXLEN = 100
 
+def load_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Load and preprocess the IMDB dataset for training a model.
 
-# TODO: maybe i should move these two var to a util/settings. Can we use pydantic here?
-def load_data():
+    The function loads the IMDB dataset, which contains movie reviews labeled as
+    positive or negative. It then pads the sequences to ensure uniform length and
+    returns the training and testing data.
+
+    Returns:
+        Tuple: A tuple containing:
+            - x_train (np.ndarray): The padded training features.
+            - y_train (np.ndarray): The training labels.
+            - x_test (np.ndarray): The padded testing features.
+            - y_test (np.ndarray): The testing labels.
+    """
     # Load the IMDB dataset
-    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=MAX_FEATURES)
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(
+        num_words=settings.MAX_FEATURES
+    )
 
     # Pad sequences to ensure uniform length
-    x_train = sequence.pad_sequences(x_train, maxlen=MAXLEN)
-    x_test = sequence.pad_sequences(x_test, maxlen=MAXLEN)
+    x_train = sequence.pad_sequences(x_train, maxlen=settings.MAXLEN)
+    x_test = sequence.pad_sequences(x_test, maxlen=settings.MAXLEN)
 
     return x_train, y_train, x_test, y_test
